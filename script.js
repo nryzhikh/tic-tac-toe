a06aaee5b32ecf3a9925bd81ab1c7a5bc5a6356e
nryzhikh <nikita.ryzhikh@outlook.com>
2023-07-21 13:53:24 +0400

    Rewrote from scratch for learning purposes Changed UI and added random O plays

--- script.js
+++ script.js
@@ -1,227 +1,175 @@
-const Gameboard = (() => {
-  let gameboard = Array(9).fill(' ');
-  const getGameboard = () => gameboard;
-  const updateGameboard = (index, playerMark) => {
-    if (gameboard[index] === ' ') {
-      gameboard[index] = playerMark;
-      return true;
-    }
-    return false;
-  };
-  const resetGameboard = () => {
-    gameboard = Array(9).fill(' ');
-  };
+const Game = (()=> {
+    let gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    let gameActive = false;
+    let currentPlayer = null;
+    let roundCounter = 1;
+    let playerXScore = 0;
+    let playerOScore = 0;
+    const playerX = 'X';
+    const playerO = 'O';
+    const gameContainer = document.getElementById('game');
+    const playerXDisplay = document.getElementById('player-x-display');
+    const playerODisplay = document.getElementById('player-o-display');
+    const roundDisplay = document.getElementById('round-display');
+    const newGameBtn = document.getElementById('new-game-btn');
+    const clearBtn = document.getElementById('clear-board-btn');
 
-  return { getGameboard, updateGameboard, resetGameboard};
+
+    const getGameboard = () => gameboard;
+
+    const startGame = () => {
+        gameActive = true;
+        currentPlayer = playerX;
+        playerXScore = 0;
+        playerOScore = 0;
+        roundCounter = 1;
+        gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+        renderGame();
+    }
+
+    const newRound = () => {
+        gameboard = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+        renderGame();
+        gameActive = true;
+        currentPlayer = playerX;
+    }
+
+    const updateGameboard = (index, currentPlayer) => {
+        if (gameboard[index] === ' ') {
+            gameboard[index] = currentPlayer;
+            return true;
+        }
+    };
+    const togglePlayer = (currentPlayer) => {
+        return currentPlayer === playerX ? playerO : playerX;
+      };
+
+    const playRound = (index) => {
+        if (gameActive === true && updateGameboard(index, currentPlayer)) {
+            if (checkWinCondition(currentPlayer) === true) {
+                updateScore();
+                endRound();
+                showPopup(`${currentPlayer} WON`)
+            } else if (checkForTie()) {
+                endRound();
+                showPopup('TIE')
+            }
+            else {
+                updateGameboard(index, currentPlayer);
+                currentPlayer = togglePlayer(currentPlayer);
+                console.log(gameboard, currentPlayer);
+            }
+        }
+    }
+
+
+    const winCombinations = [
+        [0 ,1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    const checkWinCondition = (currentPlayer) => {
+        for (let combination of winCombinations) {
+            if (
+                Game.getGameboard()[combination[0]] === currentPlayer &&
+                Game.getGameboard()[combination[1]] === currentPlayer &&
+                Game.getGameboard()[combination[2]] === currentPlayer
+            ) {
+                return true;
+            }
+        } 
+        return false;
+    };
+
+    const endRound = () => {
+        roundCounter++;
+        playerODisplay.textContent = playerOScore;
+        playerXDisplay.textContent = playerXScore;
+        roundDisplay.textContent = roundCounter;
+        gameActive = false;
+    }
+
+    const checkForTie = () => {
+        return !getGameboard().includes(' ');
+    }
+
+    const updateScore = () => {
+        if (currentPlayer === playerX) playerXScore++
+        else if (currentPlayer === playerO) playerOScore++
+    }
+
+    const renderGame = () => {
+        gameContainer.innerHTML = '';
+        getGameboard().forEach((cell, index) => {
+            const cellDiv = document.createElement('div');
+            cellDiv.textContent = cell;
+            cellDiv.dataset.index = index;
+            cellDiv.classList.add('cell-div');
+            cellDiv.addEventListener('click', handleClick)
+            gameContainer.appendChild(cellDiv);
+        });
+    };
+
+    const handleClick = (event) => {
+        if (gameActive === true && gameboard[event.target.dataset.index] === ' ') {
+            event.target.textContent = currentPlayer;
+            playRound(event.target.dataset.index);
+            if (gameActive === true) {
+                let botChoice = getRandomIndex();
+                const cellDiv = document.querySelector(`[data-index="${botChoice}"]`);
+                cellDiv.textContent = currentPlayer;
+                playRound(botChoice);
+            }
+            
+        } else if (gameActive === false) {
+            newRound();
+        }
+    }
+
+
+
+    const getRandomIndex = () => {
+        let availableCells = [];
+        getGameboard().forEach((cell, index) => {
+            if (cell === ' ') { 
+                availableCells.push(index)
+            }
+        });
+        const randomIndex = Math.floor(Math.random() * availableCells.length);
+        const selectedMove = availableCells[randomIndex]
+        return selectedMove
+    }
+
+    const popup = document.getElementById("popup");
+    const popupMessage = document.getElementById("popup-message");
+
+    // Function to show the pop-up with a specific message
+    const showPopup = (message) => {
+    popupMessage.textContent = message;
+    popup.style.display = "block";
+    }
+
+    // Function to hide the pop-up
+    const hidePopup = () => {
+    popup.style.display = "none";
+    }
+
+    // Example usage after determining game result (win or tie)
+    // Replace "playerName" and "win/tie" with appropriate values
+
+    popup.addEventListener("click", hidePopup);
+
+    newGameBtn.addEventListener('click', startGame)
+    clearBtn.addEventListener('click', newRound)
+
+    
+
+
+
+
+
+    return { getGameboard, updateGameboard, playRound, startGame, renderGame, getRandomIndex};
 })();
 
-const Player = (name, mark) => {
-  let wins = 0;
-
-  const incrementWins = () => {
-    wins++;
-  };
-
-  const getWins = () => {
-    console.log(wins);
-    return wins;
-  }
-  
-  return {name, mark, incrementWins, getWins};
-};
-
-const Game = (() => {
-  let currentPlayer;
-  let gameActive = false;
-  let round = 1;
-  const players = [
-    Player('', 'X'),
-    Player('', '0')
-  ];
-
-  const togglePlayer = (currentPlayer) => {
-    return currentPlayer === players[0] ? players[1] : players[0];
-  };
-
-  const winningCombinations = [
-    [0 ,1 ,2], [3, 4, 5], [6, 7 ,8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
-
-  const checkForWin = (mark) => {
-    for (let combination of winningCombinations) {
-      if (
-        Gameboard.getGameboard()[combination[0]] === mark &&
-        Gameboard.getGameboard()[combination[1]] === mark &&
-        Gameboard.getGameboard()[combination[2]] === mark 
-      ) {
-        return true;
-      }
-    }
-    return false;
-    };
-
-  const checkForTie = () => {
-    return !Gameboard.getGameboard().includes(' ');
-  };
-
-  const playTurn = (index) => {
-    if (Gameboard.updateGameboard (index, currentPlayer.mark) && gameActive)
-      if (checkForWin(currentPlayer.mark)) {
-        updateWinCounter(currentPlayer);
-        endGame(`${currentPlayer.name} wins!`);
-      } else if (checkForTie()) {
-        endGame('Tie!')
-      } else {
-      Gameboard.updateGameboard (index, currentPlayer.mark);
-      currentPlayer = togglePlayer(currentPlayer);
-      highlightCurrentPlayer();
-      setStatusDisplay();
-    };
-    renderGameboard();
-
-  };
-
-  const newGame = () => {
-    currentPlayer = players[0];
-    renderGameboard();
-    highlightCurrentPlayer();
-    gameActive = true;
-    hideStartButton();
-    counter();
-  }
-
-  const endGame = (message) => {
-    statusDisplay.textContent = message;
-
-    renderGameboard();
-    gameActive = false;
-
-  }
-
-  const resetGame = () => {
-    Gameboard.resetGameboard();
-    newGame();
-  }
-
-  const gameBoardDiv = document.getElementById('gameboard');
-  const statusDisplay = document.getElementById('status');
-  const startButton = document.getElementById('start-btn');
-  const player1Input = document.getElementById('player1-name');
-  const player2Input = document.getElementById('player2-name');
-  const score = document.getElementById('score');
-  const player1Score = document.getElementById('player1-score');
-  const player2Score = document.getElementById('player2-score');
-  const player1board = document.getElementById('player1-board');
-  const player2board = document.getElementById('player2-board');
-
-
-  const handleStartClick = () => {
-    const player1Name = player1Input.value.trim();
-    const player2Name = player2Input.value.trim();
-
-
-    if (player1Name !== '' && player2Name !== '') {
-      players[0].name = player1Name;
-      players[1].name = player2Name;
-      currentPlayer = players[0];
-      setStatusDisplay();
-      newGame();
-    } else {
-      alert('Please enter name for both players')
-    }
-  };
-
-  const counterCreator = () => {
-    let count = 0;
-    return () => {
-      count++;
-      score.textContent = `Round: ${count}`;
-    };
-  };
-
-  const counter = counterCreator();
-
-  const updateWinCounter = (player) => {
-    player.incrementWins();
-    if (player === players[0]) {
-      player1Score.textContent = player.getWins();
-    } else if (player === players[1]) {
-      player2Score.textContent = player.getWins();
-    }
-  };
-
-  startButton.addEventListener('click', handleStartClick);
-
-
-  const setStatusDisplay = () => {
-    statusDisplay.textContent = `Current player: ${currentPlayer.name}`;
-  };
-
-  const highlightCurrentPlayer = () => {
-    if (currentPlayer === players[0]) {
-      player1board.classList.add('current-player');
-      player2board.classList.remove('current-player');
-    } else if (currentPlayer === players[1]) {
-      player1board.classList.remove('current-player');
-      player2board.classList.add('current-player');
-    }
-  }
-
-
-  hideStartButton = () => {
-    startButton.style.display = 'none';
-  }
-
-  const renderGameboard = () => {
-    gameBoardDiv.innerHTML = '';
-    Gameboard.getGameboard().forEach((cell, index) => {
-      const cellDiv = document.createElement('div');
-      cellDiv.textContent = cell;
-      cellDiv.dataset.index = index;
-      cellDiv.addEventListener('click', (e) => {
-        if (gameActive)
-        playTurn(e.target.dataset.index)
-        else {
-          resetGame();
-        };
-      })
-      cellDiv.classList.add('cell');
-      gameBoardDiv.appendChild(cellDiv);
-    });
-    const resetDiv = document.createElement('button');
-    resetDiv.textContent = 'Clear board';
-    resetDiv.setAttribute('id', 'restart-btn');
-    resetDiv.addEventListener('click', () => {
-      Gameboard.resetGameboard();
-      renderGameboard();
-    });
-    gameBoardDiv.appendChild(resetDiv);
-  }
-
-  const botPlay = () => {
-    if(currentPlayer === players[1] && gameActive) {
-      setTimeout(()=> {
-        const availableMoves = [];
-        Gameboard.getGameboard().forEach((cell, index) => {
-          if (cell === ' ') {
-            availableMoves.push(index);
-          }
-        });
-        const randomIndex = Math.floor(Math.random() * availableMoves.length);
-        const selectedMove = availableMoves[randomIndex];
-        playTurn(selectedMove);
-      }, 500);
-    }
-  }
-
-
-
-
-
-
-
-  return {newGame};
-
-  })();
+Game.startGame();
